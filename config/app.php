a9eca521f95b5ad346f17c9f0db9183fbc30aa9a
aungzawphyo <aungzawphyo1102@gmail.com>
2023-05-05 15:27:13 +0630

    spatie permission installation

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
