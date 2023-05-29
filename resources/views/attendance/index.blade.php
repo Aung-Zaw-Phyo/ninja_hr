201b94d727b8f45d7a419ded371ceb50bac59789
aungzawphyo <aungzawphyo1102@gmail.com>
2023-05-29 11:14:33 +0630

    datatable pdf export

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('title', 'Attendance')
-
+@section('extra_css')
+    <style>
+        .button-page-length, .button-page-length.active{
+            background: #4CD195 !important;
+            color: #fff !important;
+            border: none !important;
+            border-radius: 3px !important;
+            outline: none !important;
+        }
+    </style>
+@endsection
 @section('content')
     @can('create_attendance')
         <a href="{{ route('attendance.create') }}" class="btn btn-theme"><i class="fas fa-plus-circle"></i> Create Attendance</a>
@@ -29,6 +39,96 @@
 <script>
     $(document).ready(function () {
         var table = $('.DataTable').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'pdfHtml5',
+                    text: '<i class="fas fa-file-pdf"></i> PDF',
+                    className: 'btn btn-dark',
+                    orientation: 'portrait',
+                    pageSize: 'A4',
+                    title: 'Attendance',
+                    exportOptions: {
+                        columns: [ 1, 2, 3, 4]
+                    },
+                    customize: function (doc) {
+                        doc.content.splice(0,1);
+                        var now = new Date();
+                        var datetime = now.getDate() + "/"
+                                        + (now.getMonth()+1)  + "/" 
+                                        + now.getFullYear() + " "  
+                                        + now.getHours() + ":"  
+                                        + now.getMinutes() + ":" 
+                                        + now.getSeconds();
+                        doc.pageMargins = [20,40,20,30];
+                        doc.defaultStyle.fontSize = 8;
+                        doc.styles.tableHeader.fontSize = 8;
+                        doc.styles.tableBodyEven.alignment = 'center';
+                        doc.styles.tableBodyOdd.alignment = 'center';
+
+                        doc['header']=(function() {
+                            return {
+                                columns: [
+
+                                    {
+                                        alignment: 'left',
+                                        italics: true,
+                                        text: 'Attendance',
+                                        fontSize: 12,
+                                    },
+                                    {
+                                        alignment: 'right',
+                                        fontSize: 10,
+                                        text: `Export Time : ` + datetime,
+                                    }
+                                ],
+                                margin: 20
+                            }
+                        });
+
+                        doc['footer']=(function(page, pages) {
+                            return {
+                                columns: [
+                                    {
+                                        alignment: 'left',
+                                        text: '',
+                                    },
+                                    {
+                                        alignment: 'right',
+                                        text: ['page ', { text: page.toString() },  ' of ', { text: pages.toString() }],
+                                    }
+                                ],
+                                margin: [20,0,20,10]
+                            }
+                        });
+
+                        var objLayout = {};
+                        objLayout['hLineWidth'] = function(i) { return .5; };
+                        objLayout['vLineWidth'] = function(i) { return .5; };
+                        objLayout['hLineColor'] = function(i) { return '#aaa'; };
+                        objLayout['vLineColor'] = function(i) { return '#aaa'; };
+                        objLayout['paddingLeft'] = function(i) { return 4; };
+                        objLayout['paddingRight'] = function(i) { return 4; };
+                        doc.content[0].layout = objLayout;
+                        doc.content[0].table.widths = ['20%', '20%', '30%', '30%']
+                    }
+                }, 
+                {
+                    text: "<i class='fas fa-sync'></i> Refresh",
+                    className: 'btn btn-dark mx-1',
+                    action: function (e, dt, node, config) {
+                        dt.ajax.reload(null, false);
+                    }
+                },
+                {
+                    extend: 'pageLength',
+                    className: 'btn btn-dark',
+                },
+            ],
+            lengthMenu: [
+                [10, 25, 50, 100, 500],
+                ['10 rows', '25 rows', '50 rows', '100 rows', '500 rows'],
+            ],
             responsive: true,
             processing: true,
             serverSide: true,
@@ -67,8 +167,7 @@
                     next: "<i class='fa-solid fa-circle-arrow-right'></i>",
                     previous: "<i class='fa-solid fa-circle-arrow-left'></i>"
                 },
-            }
-
+            },
         });
 
         $(document).on('click', '.delete-btn', function (e) { //This is parent to child selector to know latest render data in datatable.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,8 @@
     {{-- Datatable --}}
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.bootstrap5.min.css">
     
     {{--  Daterange Picker  --}}
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
@@ -236,6 +238,14 @@
     <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.jsdelivr.net/g/mark.js(jquery.mark.min.js)"></script>
     <script src="https://cdn.datatables.net/plug-ins/1.10.13/features/mark.js/datatables.mark.js"></script>
+
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
 
     {{--  Daterange Picker  --}}
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
